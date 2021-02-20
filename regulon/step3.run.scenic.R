ee439d27732467439e5ba34111d453627de60081
CHAN Peikai <tanpuekai@users.noreply.github.com>
2021-02-20 14:56:45 +0800

    Add files via upload

---
+++ regulon/step3.run.scenic.R
@@ -0,0 +1,39 @@
+	.libPaths("/home/groups/kathryncheah/software/R_library351")
+
+	load("int/expression_MF.RData")
+cat("###############################################################################\n")
+	#print(sessionInfo())
+cat("###############################################################################\n")
+cat(date(),": about to run SCENIC\n")
+
+
+	library(SCENIC)
+	scenicOptions <- readRDS("int/scenicOptions.Rds")
+	scenicOptions@settings$verbose <- TRUE
+	scenicOptions@settings$nCores <- 6
+	scenicOptions@settings$seed <- 123
+	#scenicOptions@dbs@500bp<- "hg19-500bp-upstream-7species.mc9nr.feather"
+	#scenicOptions@dbs@10kb<- "hg19-tss-centered-10kb-7species.mc9nr.feather"
+
+
+cat("###############################################################################\n")
+cat(date(), ": about to run runSCENIC_1_coexNetwork2modules\n")
+	
+	runSCENIC_1_coexNetwork2modules(scenicOptions)
+
+
+
+cat("###############################################################################\n")
+cat(date(),": about to run runSCENIC_2_createRegulons\n")
+
+
+	runSCENIC_2_createRegulons(scenicOptions)
+
+
+
+cat("###############################################################################\n")
+cat(date(),": about to run runSCENIC_3_scoreCells\n")
+	
+	runSCENIC_3_scoreCells(scenicOptions, exprMat_L)
+
+	cat("finished\n")
